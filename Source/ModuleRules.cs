5fafae6697e79873ca48f5677610e7b9b08bb876
Kaustubh Narkhede <narkhedekaustubh13@gmail.com>
2022-09-25 03:01:37 +0530

    python script to combine all files into one Build.cs file for unreal build system

---
+++ Source/ModuleRules.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnrealBuildTool;
+
+public class CMakeUnreal
+    : ModuleRules
+{
+    public CMakeUnreal(ReadOnlyTargetRules Target)
+        : base(Target)
+    {
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "Engine", "InputCore" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "CoreUObject", "Engine" });
+    }
+}
